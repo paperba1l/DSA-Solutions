3295053bf6e0cf82932ef86f122f2958b27acda3
Aman Nidhi <aman.sharefiles@gmail.com>
2019-09-01 23:48:18 +0530

    monor chage

--- CodeForces/dp/987C.cpp
+++ CodeForces/dp/987C.cpp
@@ -2,38 +2,39 @@
  *  @author:      skyhavoc
  *  created:      20xx
  *  https://codeforces.com/contest/987/problem/C
- *  C. Three displays
- *       
+ *  C. Three displays 
 **/
 #include <bits/stdc++.h>
 using namespace std;
- 
-const long long INF = 1e18;
- 
-struct str {
+  const long long INF = 1e18;
+  struct str
+{
     int s, c;
 };
- 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int n;
     cin >> n;
-    if (n < 3) {
-        cout <<"-1\n";
+    if (n < 3)
+    {
+        cout << "-1\n";
         return 0;
     }
-    vector <str> v(n);
-    for (int i = 0; i < n; ++i) {
+    vector<str> v(n);
+    for (int i = 0; i < n; ++i)
+    {
         cin >> v[i].s;
     }
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         cin >> v[i].c;
     }
- 
-    vector <long long> dp(n);
+      vector<long long> dp(n);
     // INIT
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         dp[i] = INF;
     }
 
@@ -42,17 +43,22 @@ int main() {
         kth element and we know the cost for the pair (ith and kth element)
         in dp[k] */
     long long ans = INF;
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (v[i].s < v[j].s) {
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (v[i].s < v[j].s)
+            {
 
                 // calculating the dps
-                if (v[i].c + v[j].c < dp[j]) {
+                if (v[i].c + v[j].c < dp[j])
+                {
                     dp[j] = v[i].c + v[j].c;
                 }
 
                 // this is actual dp logic
-                if (dp[i] + v[j].c < ans) {
+                if (dp[i] + v[j].c < ans)
+                {
                     ans = dp[i] + v[j].c;
                 }
             }
