cc46331defa740f218a8ccbf51179962f78cd4d3
Aman Nidhi <aman.sharefiles@gmail.com>
2020-06-18 22:56:23 +0530

    ib

--- interviewbit/FirstMissingInteger.cpp
+++ interviewbit/FirstMissingInteger.cpp
@@ -1,32 +1,37 @@
 /**
- *  @author:      skyhavoc
+ *  @author:      skyhavoc / Aman Nidhi
  *  created:      2019
  *  problem:      First Missing Integer interviewbit
 **/
-
-int Solution::firstMissingPositive(vector<int> &A)
+int Solution::firstMissingPositive(vector<int> &a)
 {
-    int n = A.size();
 
-    // i will increment when Ai is negetive or greater than n
-    // or a[i] points to a dubplicate number
+    int idx = 0, n = a.size();
     for (int i = 0; i < n; i++)
     {
-        if (A[i] > 0 && A[i] <= n)
+        if (a[i] <= 0)
         {
-            int pos = A[i] - 1;
-            if (A[pos] != A[i])
-            {
-                swap(A[pos], A[i]);
-                i--;
-            }
+            swap(a[i], a[idx]);
+            idx++;
         }
     }
+    if (idx == n)
+        return 1;
+    a.erase(a.begin(), a.begin() + idx);
+
+    n = a.size();
     for (int i = 0; i < n; i++)
     {
-        if (A[i] != i + 1)
-            return (i + 1);
+        if (abs(a[i]) - 1 < n and a[abs(a[i]) - 1] > 0)
+        {
+            a[abs(a[i]) - 1] *= -1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+            return i + 1;
     }
     return n + 1;
 }
-// { 6,5,1,3,7,8,4,2} first get 1 is correct place (0th index) then progress
