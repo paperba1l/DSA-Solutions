bcdaf7662074ac9655e9bbbee27e0ad4effa4aee
Aman Nidhi <aman.sharefiles@gmail.com>
2020-07-23 17:46:43 +0530

    lc

--- leetcode/45. Jump Game II.cpp
+++ leetcode/45. Jump Game II.cpp
@@ -10,26 +10,32 @@ public:
         if (nums[0] == 0)
             return -1;
 
-        int maxReach = nums[0],
-            steps = nums[0],
-            jump = 1;
+        int maxReach = nums[0], // max index you can reach so far
+            steps = nums[0],    // from an index i how many steps(i unit) you can take
+            jump = 1;           // answer variable
 
         for (int i = 1; i < n; i++)
         {
             if (i == (n - 1))
                 return jump;
 
-            maxReach = max(maxReach, i + nums[i]);
-            steps--;
+            maxReach = max(maxReach, i + nums[i]); // just track max index reachable
+            steps--;                               // if you have reached index i then you have jumped, decrement steps
 
+            // if you have reached the max reachable point from point i,
+            // then its time to take a new jump
             if (steps == 0)
             {
-                jump++;
+                jump++; // increase jump
 
+                // if we are at index i and max-reachable is lesser or equal to it self, then
+                //  cannot reach the end
                 if (maxReach <= i)
                 {
                     return -1;
                 }
+
+                // from index i count how many more (1 unit) steps are needed
                 steps = maxReach - i;
             }
         }
